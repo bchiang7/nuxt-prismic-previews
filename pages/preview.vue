11d84bb1e6f46f73841a2766922a8459b877c5ef
Brittany Chiang <brittany.chiang@gmail.com>
2019-10-01 21:14:53 -0400

    Preview works for homepage!

--- pages/preview.vue
+++ pages/preview.vue
@@ -12,15 +12,13 @@ export default {
 
   async asyncData({ app, query, redirect }) {
     const previewToken = query.token
-    console.warn('TCL: Data -> previewToken', previewToken)
-    // const api = await Prismic.getApi(PrismicConfig.apiEndpoint)
 
     const url = await app.$prismic.api.previewSession(
       previewToken,
       LinkResolver,
       '/'
     )
-    console.warn('TCL: Data -> url', url)
+
     redirect(url)
   }
 }
