92084233fd84c30f86fa8f4707311f401dec7669
Brittany Chiang <brittany.chiang@gmail.com>
2019-10-01 22:36:33 -0400

    Update link resolver

--- pages/preview.vue
+++ pages/preview.vue
@@ -5,7 +5,18 @@
 </template>
 
 <script>
-const LinkResolver = (doc, ctx) => '/'
+const LinkResolver = (doc, ctx) => {
+  if (doc.isBroken) {
+    return '/404'
+  }
+  if (doc.type === 'home') {
+    return '/'
+  }
+  if (doc.type === 'about') {
+    return '/about'
+  }
+  return '/404'
+}
 
 export default {
   name: 'Preview',
