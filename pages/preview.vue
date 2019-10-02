bb602608339fe3d6176327ed8b675d35c21a3d75
Brittany Chiang <brittany.chiang@gmail.com>
2019-10-01 21:11:51 -0400

    Try adding preview page

---
+++ pages/preview.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    Preview
+  </div>
+</template>
+
+<script>
+const LinkResolver = (doc, ctx) => '/'
+
+export default {
+  name: 'Preview',
+
+  async asyncData({ app, query, redirect }) {
+    const previewToken = query.token
+    console.warn('TCL: Data -> previewToken', previewToken)
+    // const api = await Prismic.getApi(PrismicConfig.apiEndpoint)
+
+    const url = await app.$prismic.api.previewSession(
+      previewToken,
+      LinkResolver,
+      '/'
+    )
+    console.warn('TCL: Data -> url', url)
+    redirect(url)
+  }
+}
+</script>
