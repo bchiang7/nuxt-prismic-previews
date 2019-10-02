60fcbe8d899b063051bc0590fb301be506dd1470
Brittany Chiang <brittany.chiang@gmail.com>
2019-10-01 22:47:09 -0400

    Move link resolver to its own file

--- pages/preview.vue
+++ pages/preview.vue
@@ -5,18 +5,7 @@
 </template>
 
 <script>
-const LinkResolver = (doc, ctx) => {
-  if (doc.isBroken) {
-    return '/404'
-  }
-  if (doc.type === 'home') {
-    return '/'
-  }
-  if (doc.type === 'about') {
-    return '/about'
-  }
-  return '/404'
-}
+import LinkResolver from '@/plugins/link-resolver'
 
 export default {
   name: 'Preview',
---
+++ plugins/link-resolver.js
@@ -0,0 +1,14 @@
+const LinkResolver = (doc, ctx) => {
+  if (doc.isBroken) {
+    return '/404'
+  }
+  if (doc.type === 'home') {
+    return '/'
+  }
+  if (doc.type === 'about') {
+    return '/about'
+  }
+  return '/404'
+}
+
+export default LinkResolver
