c6ddab0cb72c762b09b76f7176811eae0946c553
Brittany Chiang <brittany.chiang@gmail.com>
2019-10-03 23:53:26 -0400

    Clean up middleware

--- middleware/preview.js
+++ middleware/preview.js
@@ -1,25 +1,12 @@
-/* eslint-disable */
 import Prismic from 'prismic-javascript'
-import PrismicConfig from '~/prismic.config.js'
+import { getApi } from '~/utils'
 import LinkResolver from '~/plugins/link-resolver.js'
 
 export default async function(context) {
-  const api = await Prismic.getApi(PrismicConfig.apiEndpoint)
-
-  // Universal keys
   // https://nuxtjs.org/api/context/
-  const {
-    app,
-    store,
-    route,
-    params,
-    query,
-    env,
-    isDev,
-    isHMR,
-    redirect,
-    error
-  } = context
+  const { route, query, redirect } = context
+
+  const api = await getApi()
 
   // Preview support
   // https://github.com/jamespegg/prismic-nuxt/blob/0393d45810b426059d81a0347ebf9633fb94ed07/src/plugin.js#L65
