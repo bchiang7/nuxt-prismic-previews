82570a5351acbdb8db4f1bea39cac904456c9067
Brittany Chiang <brittany.chiang@gmail.com>
2019-10-06 18:31:16 -0400

    Add method to onCreate mixin

--- mixins/onCreate.js
+++ mixins/onCreate.js
@@ -2,4 +2,12 @@ export default {
   created() {
     this.refetchPageForPreview();
   },
+
+  methods: {
+    refetchPageForPreview() {
+      console.warn(
+        'You must have a refetchPageForPreview method in order for Prismic Previews to work!',
+      );
+    },
+  },
 };
